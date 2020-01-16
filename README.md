183166c4a9d61fbc0b006e408bef4140c7adbaa2
Dorota Żelazkiewicz <dorota.elen@gmail.com>
2020-01-16 10:25:26 +0100

    script deleting spaces

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # InDesign-script
+This is custom script for InDesign that removes spaces in calog numbers in tables. Was made for 
+specific customer catalog.
---
+++ gs_catalog_numbers.jsx
@@ -0,0 +1,21 @@
+﻿var document = app.activeDocument;
+alert('start skryptu');
+for (var currentPageIndex = 0; currentPageIndex < document.pages.length; currentPageIndex++){
+        var currentPage = document.pages.item(currentPageIndex);
+         for (var currentFrameIndex = 0; currentFrameIndex < currentPage.textFrames.length; currentFrameIndex++) {
+            var currentFrame = currentPage.textFrames[currentFrameIndex];
+            for(var currentTableIndex = 0; currentTableIndex < currentFrame.tables.length; currentTableIndex++){
+                var currentTable = currentFrame.tables.item(currentTableIndex);
+                for(currentRow = 0; currentRow < currentTable.rows.length-1; currentRow++){                    
+                    if (currentTable.rows[currentRow].cells.item(currentTable.rows[currentRow].cells.length-2).texts.item(0).contents.indexOf("katalogowy") !== -1 ) { 
+                        for (var currentRowIndex = currentRow+1; currentRowIndex < currentTable.rows.length; currentRowIndex++) {                      
+                            var currentCell = currentTable.rows[currentRowIndex].cells[currentTable.rows[currentRowIndex].cells.length-2]; 
+                             currentCell.texts.item(0).contents = currentCell.texts.item(0).contents.replace(/\s/g, "");
+                             }
+                      } ;
+                  };
+              };
+            };
+        };
+alert('koniec działania skryptu');
+
